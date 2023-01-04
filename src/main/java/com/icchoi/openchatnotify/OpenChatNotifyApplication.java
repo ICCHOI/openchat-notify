59983bc62d8539414a57e01e8d48e036982dff98
InChang Choi <cicblck@gmail.com>
2023-01-04 23:20:37 +0900

    :tada: init : 프로젝트 생성 (#1)
    프로젝트 생성 입니다.

---
+++ src/main/java/com/icchoi/openchatnotify/OpenChatNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.icchoi.openchatnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenChatNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenChatNotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icchoi/openchatnotify/OpenChatNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icchoi.openchatnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenChatNotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
