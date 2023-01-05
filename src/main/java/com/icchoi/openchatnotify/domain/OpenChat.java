f50e00652926b3feaa07bdf287f67ff92e82488e
InChang Choi <cicblck@gmail.com>
2023-01-05 23:59:27 +0900

    :sparkles: feat : 오픈채팅 도메인 작성 (#2)

---
+++ src/main/java/com/icchoi/openchatnotify/domain/OpenChat.java
@@ -0,0 +1,43 @@
+package com.icchoi.openchatnotify.domain;
+
+public class OpenChat {
+    String success;
+    Result result;
+    String node;
+
+    public OpenChat() {
+    }
+
+    public OpenChat(String success, Result result, String node) {
+        this.success = success;
+        this.result = result;
+        this.node = node;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public String getNode() {
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        return "OpenChat{" +
+                "success='" + success + '\'' +
+                ", result=" + result.toString() +
+                ", node='" + node + '\'' +
+                '}';
+    }
+
+    // TODO 외부 입력값을 받아 최종 인원을 계산 할 수 있도록 개선
+    public boolean isHeadcountFull() {
+        return result.getHeadcount() < 1500;
+    }
+}
+
