b6b8ffef203ad5f834e11e11a804c319ed6b9294
ICCHOI <cicblck@gmail.com>
2023-03-30 15:47:37 +0900

    :recycle: refactor : 알림 서비스의 위치를 조정 (#7)
    Notifier 이름 변경 후 패키지 위치 조정

--- src/main/java/com/icchoi/openchatnotify/infra/message/slack/service/MessageService.java
+++ src/main/java/com/icchoi/openchatnotify/infra/message/slack/service/MessageService.java
@@ -1,4 +1,4 @@
-package com.icchoi.openchatnotify.infra.sms.slack.service;
+package com.icchoi.openchatnotify.infra.message.slack.service;
 
 public interface MessageService {
     void postMessage(String message);
